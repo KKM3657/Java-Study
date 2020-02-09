55c193304d302018535adb9e41824b3c207e5d80
KKM3657 <mmm3657@naver.com>
2020-02-09 23:54:46 +0900

    7과 다형성

---
+++ exam01/KumhoTire.java
@@ -0,0 +1,23 @@
+package sec02.exam01;
+
+public class KumhoTire extends Tire {
+	//필드
+	//생성자
+	public KumhoTire(String location, int maxRotation) {
+		super(location , maxRotation);
+	}
+	
+	@Override
+	public boolean roll() {
+		++accumulatedRotation;
+		if(accumulatedRotation < maxRotation) {
+			System.out.println(location +" HankookTire 수명: " +
+				(maxRotation-accumulatedRotation)+ "회");
+		return true;
+		} else {
+			System.out.println("***" + location + " HankookTire 펑크 ***");
+			return false;
+		}
+	}
+}
+
